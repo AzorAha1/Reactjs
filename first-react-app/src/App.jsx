2b76888e3c94eaf80714d2ee4702d2b322782cad
faiz <wizfaiz@icloud.com>
2024-07-31 07:02:54 +0100

    update

--- first-react-app/src/App.jsx
+++ first-react-app/src/App.jsx
@@ -1,12 +1,12 @@
 import Card from './Card.jsx';
-
+import List from './List.jsx';
 function App() {
   return (
     <>
       <Card name='Mohammed Faisal Adamu' title={true} EmploymentStatus={true}></Card>
       <Card name='Mr Charles' title={false} EmploymentStatus={false}></Card>
       <Card></Card>
-      
+      <List></List>
     </>
   );
 }
