355423a0daf24f04ec787b543aef30a7de9dc6e6
faiz <wizfaiz@icloud.com>
2024-08-02 21:34:24 +0100

    counter app done

---
+++ countdown/src/Counter.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+
+function Counter(props) {
+    const [count, UseCount] = useState(0)
+    const IncrementCount = () => {
+        UseCount(count + 1)
+    } 
+    const DecrementCount = () => {
+        UseCount(count - 1)
+    }
+    const ResetCount = () => {
+        UseCount(0)
+    }
+    return (
+        <>
+            <div className='counter'>
+                {count}
+            </div>
+            <div className='button-container'>
+                <button className='button' onClick={() => IncrementCount()}>press me</button>
+                <button className='button' onClick={() => ResetCount()}>press me</button>
+                <button className='button' onClick={() => DecrementCount()}>press me</button>
+            </div>
+            
+        </>
+    );
+}
+export default Counter;
